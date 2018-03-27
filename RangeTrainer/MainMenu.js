4da7f9797f443e3120ab1fc8625a87fba842cf04
Nicholas Kyle <nickdotkyle@hotmail.com>
2018-03-27 10:40:59 +0100

    Navigation added

--- RangeTrainer/MainMenu.js
+++ RangeTrainer/MainMenu.js
@@ -1,15 +1,64 @@
 import React, { Component } from "react";
 import { View, Text, Button } from "react-native";
+import { StackNavigator } from "react-navigation";
 import RangeTrainer from "./RangeTrainer";
 import RangeCreator from "./RangeCreator";
 
-export default class MainMenu extends Component {
+class MainMenuScreen extends Component {
 
     render(){
         return(
-            <View>
-                <Text>Bonjour</Text>
+            <View style={{ flex: 1, alignItems: "center"}}>
+                <Text>Main Menu</Text>
+                <Button
+                    title="Start training"
+                    onPress={ () => this.props.navigation.navigate("RT")}
+                />
+                <Button
+                    title="Create new range"
+                    onPress={ () => this.props.navigation.navigate("RC")}
+                />
             </View>
-        )
+        );
     }
 }
+
+class RangeTrainerScreen extends Component {
+    render(){
+        return (
+            <RangeTrainer />
+        );
+    }
+}
+
+class RangeCreatorScreen extends Component {
+    render(){
+        return (
+            <RangeCreator />
+        );
+    }
+}
+
+const RootStack = StackNavigator(
+    {
+        Main: {
+            screen: MainMenuScreen,
+        },
+        RT: {
+            screen: RangeTrainerScreen,
+        },
+        RC: {
+            screen: RangeCreatorScreen,
+        }
+    },
+    {
+        initialRouteName: "Main",
+    }
+);
+
+export default class MainMenu extends Component {
+    render() {
+        return <RootStack />;
+    }
+}
+
