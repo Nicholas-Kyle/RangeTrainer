dc1f4172c8f8e5869dbf94f293c2c6091f3fc99a
laurie <laurie@brandwatch.com>
2018-03-28 17:22:20 +0100

    Move and rename user range to RangeAdder

--- RangeTrainer/RangeAdder/RangeAdder.js
+++ RangeTrainer/RangeAdder/RangeAdder.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import { View } from "react-native";
+import parseUserRange from "./parseUserRange";
 
 class RangeAdder extends Component {
+  /**R
+   * // for testing in browser can be removed once integrated
+  var customRangeString = prompt("Please enter a range");
+
+customRangeString = customRangeString.replace(/\s+/g, "");
+var customRangeArray = customRangeString.split(",");
+   */
   render() {
     return <View />;
   }
