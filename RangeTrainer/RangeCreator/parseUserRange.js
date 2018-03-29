b6f1b592ed3d823279218aa411ad927b14556ded
laurie <laurie@brandwatch.com>
2018-03-29 14:20:57 +0100

    Make helpers file and refactor parseUserRange

--- RangeTrainer/RangeCreator/parseUserRange.js
+++ RangeTrainer/RangeCreator/parseUserRange.js
@@ -1,59 +1,65 @@
 import rangeGroups from "./rangeGroups";
-const { connectors, highCardsAndPairs } = rangeGroups;
+import { getSubgroupFromHand } from "../helpers/helpers";
 
-function parseUserRange(customRangeArray) {
-  var finalRange = [];
-  for (i = 0; i < customRangeArray.length; i++) {
-    var userHand = customRangeArray[i];
-    var currentRange = [];
-    if (userHand.indexOf("+") !== -1) {
-      userHand = userHand.replace("+", "");
+let { connectors, highCardsAndPairs } = rangeGroups;
 
-      if (userHand.indexOf("_") !== -1) {
-        userHand = userHand.replace("_", "");
+const SYMBOLS = {
+  PLUS: "+",
+  CONNECTOR: "_"
+};
 
-        for (j = 0; j < connectors.length; j++) {
-          for (k = 0; k < connectors[j].length; k++) {
-            var rangeHand = connectors[j][k];
-            if (rangeHand.toUpperCase() === userHand.toUpperCase()) {
-              for (l = 0; l <= k; l++) {
-                currentRange.push(connectors[j][l]);
-              }
-            }
-          }
-        }
-      } else {
-        for (j = 0; j < highCardsAndPairs.length; j++) {
-          for (k = 0; k < highCardsAndPairs[j].length; k++) {
-            var rangeHand = highCardsAndPairs[j][k];
+function isSingleHand(hand) {
+  return !hand.includes(SYMBOLS.PLUS);
+}
 
-            if (rangeHand.toUpperCase() === userHand.toUpperCase()) {
-              for (l = 0; l <= k; l++) {
-                currentRange.push(highCardsAndPairs[j][l]);
-              }
-            }
-          }
-        }
+function isConnectorHand(hand) {
+  return hand.includes(SYMBOLS.CONNECTOR);
+}
+
+function getRangeGroup(group, userHand) {
+  return group.find(handGroup =>
+    handGroup.some(hand => hand.toUpperCase() === userHand.toUpperCase())
+  );
+}
+
+function getHandFromRangeGroup(rangeGroup, userHand) {
+  return rangeGroup.find(hand => hand.toUpperCase() === userHand.toUpperCase());
+}
+
+function parseUserRange(userRange) {
+  let finalRange = userRange.reduce((finalRange, userHand) => {
+    if (isSingleHand(userHand)) {
+      let rangeGroup = getRangeGroup(highCardsAndPairs, userHand);
+      if (rangeGroup) {
+        let hand = getHandFromRangeGroup(rangeGroup, userHand);
+
+        return finalRange.concat(hand);
       }
-    } else {
-      for (j = 0; j < highCardsAndPairs.length; j++) {
-        for (k = 0; k < highCardsAndPairs[j].length; k++) {
-          var rangeHand = highCardsAndPairs[j][k];
 
-          if (rangeHand.toUpperCase() === userHand.toUpperCase()) {
-            currentRange.push(highCardsAndPairs[j][k]);
-          }
-        }
-      }
+      console.error("invalid hand", userHand);
+      return finalRange;
     }
-    for (j = 0; j < currentRange.length; j++) {
-      if (!finalRange.includes(currentRange[j])) {
-        finalRange.push(currentRange[j]);
-      }
-    }
-  }
 
-  return finalRange;
+    userHand = userHand.replace("+", "");
+    if (isConnectorHand(userHand)) {
+      userHand = userHand.replace("_", "");
+
+      let rangeGroup = getRangeGroup(connectors, userHand);
+
+      return finalRange.concat(...getSubgroupFromHand(userHand, rangeGroup));
+    }
+
+    let rangeGroup = getRangeGroup(highCardsAndPairs, userHand);
+    if (rangeGroup) {
+      return finalRange.concat(...getSubgroupFromHand(userHand, rangeGroup));
+    }
+
+    console.error("invalid hand", userHand);
+
+    return finalRange;
+  }, []);
+
+  return [...new Set(finalRange)];
 }
 
 export default parseUserRange;
